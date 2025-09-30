cbd5986255343cef2765dfb2261136c30a74270a
nikfrol2014 <nikitka_frolov_2014@inbox.ru>
2025-09-30 18:42:26 +0300

    test-cmmit - 0

--- src/main/java/Frolov_back/NAILS_WEB_APP/NailsWebAppApplication.java
+++ src/main/java/Frolov_back/NAILS_WEB_APP/NailsWebAppApplication.java
@@ -8,6 +8,9 @@ public class NailsWebAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NailsWebAppApplication.class, args);
+
+
+		///     erekwlorjewireiw
 	}
 
 }
