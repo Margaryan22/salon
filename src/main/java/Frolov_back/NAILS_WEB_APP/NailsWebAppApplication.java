f57c0a12226c6bce242404e22095d626e8b0c0d9
nikfrol2014 <nikitka_frolov_2014@inbox.ru>
2025-09-30 18:36:54 +0300

    first commit for a back

---
+++ src/main/java/Frolov_back/NAILS_WEB_APP/NailsWebAppApplication.java
@@ -0,0 +1,13 @@
+package Frolov_back.NAILS_WEB_APP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NailsWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NailsWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Frolov_back/NAILS_WEB_APP/NailsWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package Frolov_back.NAILS_WEB_APP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NailsWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
